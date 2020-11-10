e62c5afd38839ba659b120e8e6678b34ba2c6250
Luhundun <57153251@qq.com>
2020-11-10 21:19:00 +0800

    11.10 添加了一个Toast

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public  static final String EXTRA_MESSAGE = "com.example.first_app.MESSAGE";
@@ -14,32 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //@description 为开始学习按钮创建一个点击就会收到的Toast
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "你还不能学习哦", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     protected void onPause()
     {
         super.onPause();
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         Log.d("MainActivity","修改了文本");
         editText.setHint("Welcome back");
     }
     
 
-
-    /**
-     * @description 
-     * @param 
-     * @return 
-     * @author Lu Ning
-     * @time 2020/11/10 20:20
-     */
     public void SendMessage(View view)
     {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         Log.d("MainActivity","点击了发送消息按钮");          //日志
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivity(intent);
     }
+
+
 }
