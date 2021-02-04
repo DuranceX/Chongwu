decacd15947ceec9c07a69de824b4e03d2c7502d
Cardy <1124804361@qq.com>
2021-02-04 18:03:29 +0800

    切换目录

--- chongwu/android/app/src/main/kotlin/com/example/chongwu/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chongwu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- chongwu/lib/values/Texts.dart
+++
@@ -1,6 +0,0 @@
-class Texts{
-  static const String shop = "商店";
-  static const String setting = "设置";
-  static const String mine = "我的";
-  static const String feedback = "反馈";
-}
